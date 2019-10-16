c6d7b9719fe9033f07a728d2c97883ad29a36214
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-10-16 16:19:57 +0100

    test 2 pass

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array1, array2)
   return array1.length + array2.length
 end
+
+def sum_array(numbers)
+  sum = 0
+  for number in numbers
+    sum += number
+  end
+  return sum
+end
